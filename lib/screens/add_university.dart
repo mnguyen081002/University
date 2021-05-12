3c87f28b5ed1541b4932f75ba757e6d08b1d3994
Minh Nguyen <3lang@gmail.com>
2021-05-12 17:54:13 +0700

    commit

--- lib/screens/add_university.dart
+++ lib/screens/add_university.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
+import 'package:university_helper/services/a.dart';
 
 import '../custom_search.dart';
 import '../providers/dataMajorsProvider.dart';
@@ -30,12 +31,6 @@ class _AddUniversityState extends State<AddUniversity> {
 
   List<String> _values = [];
   List<String> listDataMajors = [];
-
-  _onDelete(index) {
-    setState(() {
-      _values.removeAt(index);
-    });
-  }
 
   Future<void> getData() async {
     try {
@@ -77,7 +72,6 @@ class _AddUniversityState extends State<AddUniversity> {
     await FirebaseFirestore.instance
         .collection('ListUniversity')
         .add(university);
-    print('Done');
   }
 
   @override
@@ -109,57 +103,14 @@ class _AddUniversityState extends State<AddUniversity> {
                   decoration: InputDecoration(labelText: 'Nhập mã trường'),
                   onSaved: (value) => _maTruong = value!,
                 ),
-                Row(
-                  children: [
-                    Text('Là đại học quốc gia ?'),
-                    Switch(
-                        value: isNationalUniversity,
-                        onChanged: (value) {
-                          setState(() {
-                            isNationalUniversity = value;
-                          });
-                        }),
-                  ],
-                ),
+                _Switch(),
                 SearchPage(
-                    delegate: Data(
+                    delegate: SearchMajorsDelegate(
                   itemList: listDataMajors,
                   hintText: 'Chọn ngành',
                   scaffoldCtx: _scaffoldKey.currentContext,
                 )),
-                Consumer<DataMajorsProvider>(
-                  builder: (context, majors, child) {
-                    return majors.listMajors.isNotEmpty
-                        ? Column(
-                            children: [
-                              Text('Ngành đã thêm :' +
-                                  '${majors.listMajors.length}'),
-                              ListView.builder(
-                                shrinkWrap: true,
-                                itemBuilder: (context, index) => Container(
-                                  child: Row(
-                                    children: [
-                                      Text(
-                                        '${index + 1}.' +
-                                            majors.listMajors[index].name,
-                                      ),
-                                      IconButton(
-                                        icon: Icon(Icons.clear),
-                                        onPressed: () {
-                                          majors.removeMajors(
-                                              majors.listMajors[index]);
-                                        },
-                                      )
-                                    ],
-                                  ),
-                                ),
-                                itemCount: majors.listMajors.length,
-                              ),
-                            ],
-                          )
-                        : Container();
-                  },
-                ),
+                _buildListSelectedMajors(),
                 TextFormField(
                   decoration: InputDecoration(
                       labelText: 'Hình thức đào tạo (Tư,Công,...'),
@@ -202,218 +153,52 @@ class _AddUniversityState extends State<AddUniversity> {
       ]),
     );
   }
-}
 
-const _dsKhoiThi = [
-  'A00',
-  'A01',
-  'A02',
-];
-
-class Data extends CustomSearchDelegate<String> {
-  Data({
-    required this.itemList,
-    required String? hintText,
-    required this.scaffoldCtx,
-  }) : super(
-          searchFieldLabel: hintText,
-        );
-  final List<String> itemList;
-  final BuildContext? scaffoldCtx;
-  @override
-  List<Widget> buildActions(BuildContext context) {
-    return [Container()];
-  }
-
-  @override
-  Widget buildLeading(BuildContext context) {
-    return Container();
-  }
-
-  @override
-  Widget buildResults(BuildContext context) {
-    return Container();
-  }
-
-  @override
-  Widget buildSuggestions(BuildContext context) {
-    final suggestList = [
-      ...itemList.where(
-          (element) => element.toLowerCase().startsWith(query.toLowerCase()))
-    ];
-    return Card(
-      child: Container(
-        color: Colors.grey[100],
-        child: ListView.separated(
-          shrinkWrap: true,
-          itemBuilder: (context, index) {
-            return query.isNotEmpty
-                ? ListTile(
-                    onTap: () => buildShowInputMajors(
-                      scaffoldCtx!,
-                      suggestList[index],
-                    ),
-                    title: Text(suggestList[index]
-                        // itemList[index]['majors'],
-                        ),
-                  )
-                : Container();
-          },
-          itemCount: suggestList.length > 5 ? 5 : suggestList.length,
-          separatorBuilder: (BuildContext context, int index) {
-            return Divider(
-              height: 3,
-            );
-          },
-        ),
-      ),
+  Row _Switch() {
+    return Row(
+      children: [
+        Text('Là đại học quốc gia ?'),
+        Switch(
+            value: isNationalUniversity,
+            onChanged: (value) {
+              setState(() {
+                isNationalUniversity = value;
+              });
+            }),
+      ],
     );
   }
 
-  Future buildShowInputMajors(BuildContext scaffoldCtx, String nameMajors) {
-    List<String> khoiDaChon = [];
-    final idMajorsCtl = TextEditingController();
-    final studyTimeMajorsCtl = TextEditingController();
-    final khoiThiCtl = TextEditingController();
-
-    void addListSelected(
-        TextEditingController textEditCtl, List list, FocusNode focus) {
-      if (textEditCtl.text.isNotEmpty) {
-        list.add(textEditCtl.text);
-        textEditCtl.clear();
-        focus.unfocus();
-      }
-    }
-
-    void submit() {
-      print(khoiDaChon);
-      print(idMajorsCtl.text);
-      print(nameMajors);
-      print(studyTimeMajorsCtl.text);
-      Provider.of<DataMajorsProvider>(scaffoldCtx, listen: false).addMajors(
-        grade: khoiDaChon,
-        idMajors: idMajorsCtl.text,
-        name: nameMajors,
-        studyTime: studyTimeMajorsCtl.text,
-      );
-      Navigator.pop(scaffoldCtx);
-    }
-
-    return showModalBottomSheet(
-      context: scaffoldCtx,
-      builder: (context) {
-        final focus = FocusNode();
-        return Container(
-          padding: EdgeInsets.all(20),
-          height: 800,
-          child: Form(
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  Text(
-                    'Điền thông tin ngành',
-                    style: Theme.of(context).textTheme.headline6,
-                  ),
-                  Text('Tên Ngành : $nameMajors'),
-                  TextFormField(
-                    controller: idMajorsCtl,
-                    decoration: InputDecoration(
-                      labelText: 'Nhập mã ngành',
-                    ),
-                  ),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.end,
+  Consumer<DataMajorsProvider> _buildListSelectedMajors() {
+    return Consumer<DataMajorsProvider>(builder: (context, majors, child) {
+      return majors.listMajors.isNotEmpty
+          ? Column(children: [
+              Text('Ngành đã thêm :' + '${majors.listMajors.length}'),
+              ListView.builder(
+                shrinkWrap: true,
+                itemBuilder: (context, index) => Container(
+                  child: Row(
                     children: [
-                      Expanded(
-                        child: TextField(
-                          onSubmitted: (_) {
-                            addListSelected(khoiThiCtl, khoiDaChon, focus);
-                          },
-                          focusNode: focus,
-                          controller: khoiThiCtl,
-                          decoration: InputDecoration(
-                            labelText: 'Nhập khối thi',
-                          ),
-                        ),
+                      Text(
+                        '${index + 1}.' + majors.listMajors[index].name,
                       ),
                       IconButton(
-                        icon: Icon(Icons.add),
-                        onPressed: () => addListSelected(
-                          khoiThiCtl,
-                          khoiDaChon,
-                          focus,
-                        ),
+                        icon: Icon(Icons.clear),
+                        onPressed: () {
+                          majors.removeMajors(majors.listMajors[index]);
+                        },
                       )
                     ],
                   ),
-                  Container(
-                    constraints: BoxConstraints(maxHeight: 100),
-                    child: GridView.builder(
-                      itemBuilder: (context, index) {
-                        return _Chip(
-                          label: khoiDaChon[index],
-                          onDeleted: (index) {},
-                          index: index,
-                        );
-                      },
-                      itemCount: khoiDaChon.length,
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 4,
-                        childAspectRatio: 4 / 2,
-                        crossAxisSpacing: 0,
-                        mainAxisSpacing: 0,
-                      ),
-                    ),
-                  ),
-                  TextFormField(
-                    controller: studyTimeMajorsCtl,
-                    decoration: InputDecoration(
-                      labelText: 'Nhập thời gian đào tạo',
-                    ),
-                  ),
-                  SizedBox(height: 30),
-                  ElevatedButton(
-                    onPressed: () {
-                      submit();
-                    },
-                    child: Text('Nhập'),
-                  )
-                ],
+                ),
+                itemCount: majors.listMajors.length,
               ),
-            ),
-          ),
-        );
-      },
-    );
+            ])
+          : Container();
+    });
   }
 }
 
-class _Chip extends StatelessWidget {
-  const _Chip({
-    required this.label,
-    required this.onDeleted,
-    required this.index,
-  });
-
-  final String label;
-  final ValueChanged<int> onDeleted;
-  final int index;
-
-  @override
-  Widget build(BuildContext context) {
-    return Chip(
-      labelPadding: const EdgeInsets.only(left: 8.0),
-      label: Text(label),
-      deleteIcon: Icon(
-        Icons.close,
-        size: 20,
-      ),
-      onDeleted: () {
-        onDeleted(index);
-      },
-    );
-  }
-}
 // Row(
 // children: [
 // DropdownButton(

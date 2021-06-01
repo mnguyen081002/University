cb10d43b298c5315e570b7029045f6c82b2d74bd
Minh Nguyen <3lang@gmail.com>
2021-06-01 15:46:28 +0700

    created tabbarview

--- lib/utils/widgets/search_bar.dart
+++ lib/utils/widgets/search_bar.dart
@@ -1,58 +1,39 @@
 import 'package:flutter/material.dart';
 
+import '../constants.dart';
+
 class SearchBar extends StatelessWidget {
   const SearchBar({
     Key? key,
-    this.hintText = 'Search',
+    required this.hintText,
   }) : super(key: key);
   final String hintText;
   @override
   Widget build(BuildContext context) {
-    const double actionSize = 100;
     Size size = MediaQuery.of(context).size;
+
     return GestureDetector(
       onTap: () {},
-      child: Row(
-        children: [
-          Container(
-            height: 40,
-            width: size.width - actionSize,
-            child: TextField(
-              enabled: false,
-              decoration: InputDecoration(
-                border: UnderlineInputBorder(
-                  borderRadius: BorderRadius.circular(5),
-                  borderSide: BorderSide(
-                    width: 0,
-                    style: BorderStyle.none,
-                  ),
-                ),
-                prefixIcon: Icon(Icons.search),
-                filled: true,
-                fillColor: Colors.grey[100],
-                hintStyle: TextStyle(color: Colors.grey[600]),
-                hintText: hintText,
+      child: Container(
+        height: 40,
+        width: size.width - actionSize,
+        child: TextField(
+          enabled: false,
+          decoration: InputDecoration(
+            border: UnderlineInputBorder(
+              borderRadius: BorderRadius.circular(5),
+              borderSide: BorderSide(
+                width: 0,
+                style: BorderStyle.none,
               ),
             ),
+            prefixIcon: Icon(Icons.search),
+            filled: true,
+            fillColor: Colors.grey[100],
+            hintStyle: TextStyle(color: Colors.grey[600]),
+            hintText: hintText,
           ),
-          TextButton(
-            onPressed: () {},
-            child: Row(
-              children: [
-                Icon(
-                  Icons.filter_list_alt,
-                  color: Theme.of(context).accentColor,
-                ),
-                Text(
-                  'Lọc',
-                  style: TextStyle(
-                    color: Theme.of(context).accentColor,
-                  ),
-                ),
-              ],
-            ),
-          )
-        ],
+        ),
       ),
     );
   }
